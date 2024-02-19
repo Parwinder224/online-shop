8c47dbc1165dfb2bb5da65298b243d1b559624c3
Parwinder <kauraparwinder@gmail.com>
2024-02-19 10:24:39 +0530

    header footer changes

--- src/common-component/header/header.component.ts
+++ src/common-component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @ViewChild('navbarToggler') navbarToggler!: ElementRef;
+  @ViewChild('navbarNav') navbarNav!: ElementRef;
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    if (this.navbarToggler && this.navbarNav) {
+      const toggler = this.navbarToggler.nativeElement;
+      const target = this.navbarNav.nativeElement;
+
+      toggler.addEventListener('click', () => {
+        if (target.classList.contains('show')) {
+          target.classList.remove('show');
+        } else {
+          target.classList.add('show');
+        }
+      });
+    }
+  
+  }
 }
